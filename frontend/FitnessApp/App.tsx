161333ca7681b1ec3eb3cb6d008fa77e6810cbaa
Parker Bedlan <parkerbedlan@gmail.com>
2021-09-16 15:21:14 -0500

    basic layout for login page

--- frontend/FitnessApp/App.tsx
+++ frontend/FitnessApp/App.tsx
@@ -1,17 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ThemeProvider, Button, Input, Text } from "react-native-elements";
-import useSWR from "swr";
+import {
+  ThemeProvider,
+  Button,
+  Input,
+  Text,
+  Icon,
+} from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
+import { Formik } from "formik";
 
 export default function App() {
-  const [userInput, setUserInput] = useState("");
-
-  const { message, isLoading } = useHelloWorld();
-  const [isVisibleHelloWorld, setIsVisibleHelloWorld] = useState(true);
-
   return (
     <SafeAreaProvider>
       <ThemeProvider>
@@ -20,34 +20,34 @@ export default function App() {
         >
           <Text style={tw`text-3xl`}>Welcome to Fitness Dium!</Text>
           <LineBreak />
-          <StatusBar style="auto" />
-          <Input
-            placeholder="Your Input Here"
-            value={userInput}
-            onChangeText={setUserInput}
-          />
-          <Button
-            raised
-            onPress={() => setIsVisibleHelloWorld(!isVisibleHelloWorld)}
-            title="Press Me"
-            buttonStyle={tw`w-48 h-24 bg-purple-500`}
-            titleStyle={tw`text-3xl`}
-          />
-          {isVisibleHelloWorld && (
-            <View
-              style={{
-                borderWidth: 1,
-                margin: "5%",
-                padding: "5%",
-                width: "80%",
-                alignItems: "center",
-              }}
-            >
-              <Text h1 style={tw`text-red-500`}>
-                {isLoading ? "Loading..." : message}
-              </Text>
-            </View>
-          )}
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => console.log("logging in with", values)}
+          >
+            {({ values, handleChange, handleSubmit }) => (
+              <>
+                <Input
+                  placeholder="john@example.com"
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  label="Email"
+                  leftIcon={<Icon name="email" />}
+                />
+                <Input
+                  placeholder="password123"
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  label="Password"
+                  secureTextEntry={true}
+                  leftIcon={<Icon name="lock" />}
+                />
+                <LoginSignupButtonGroup
+                  handleSubmit={handleSubmit}
+                  version={2}
+                />
+              </>
+            )}
+          </Formik>
         </View>
       </ThemeProvider>
     </SafeAreaProvider>
@@ -58,46 +58,54 @@ const LineBreak = () => {
   return <Text>{"\n"}</Text>;
 };
 
-const useHelloWorld = () => {
-  const { data, error, isValidating, mutate } = useSWR<HelloWorldType, string>(
-    "https://jsonplaceholder.typicode.com/users/1",
-    fetcher
-  );
-
-  return {
-    message: data ? data.address.city : null,
-    isLoading: !data && !error,
-    isError: error,
+const LoginSignupButtonGroup = ({
+  handleSubmit,
+  version,
+}: {
+  handleSubmit: any;
+  version: 1 | 2;
+}) => {
+  const versions = {
+    1: (
+      <View
+        style={tw`flex flex-row w-full justify-center items-center justify-around mt-2`}
+      >
+        <Button
+          raised
+          title="Don't have an account? Sign Up!"
+          buttonStyle={tw`bg-purple-500 w-44 h-20`}
+          titleStyle={tw`text-lg`}
+          onPress={(e) => console.log("signing up")}
+        />
+        <Button
+          raised
+          title="Log in"
+          buttonStyle={tw`bg-purple-500 w-44 h-20`}
+          titleStyle={tw`text-3xl p-3`}
+          onPress={(e) => handleSubmit()}
+        />
+      </View>
+    ),
+    2: (
+      <View style={tw`flex justify-center items-center`}>
+        <Button
+          raised
+          title="Log in"
+          buttonStyle={tw`bg-purple-500 w-44 h-20`}
+          titleStyle={tw`text-3xl p-3`}
+          onPress={(e) => handleSubmit()}
+        />
+        <View style={tw`mt-1`}>
+          <Button
+            type="clear"
+            title="Don't have an account? Sign Up!"
+            buttonStyle={tw`h-20`}
+            titleStyle={tw`text-lg text-purple-800`}
+            onPress={(e) => console.log("signing up")}
+          />
+        </View>
+      </View>
+    ),
   };
+  return versions[version];
 };
-
-type HelloWorldType = {
-  address: { city: string };
-};
-
-const fetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json());
-
-// unnecessary, just used as an example of a StyleSheet equivalent using tailwind if we end up wanting to go this route
-const twStyles = {
-  container: tw`bg-gray-50 h-full w-full flex justify-center items-center`,
-  title: tw`text-3xl`,
-  testButton: tw`w-48 h-24 bg-purple-500`,
-};
-
-// use inline tailwind instead https://tailwindcss.com/docs
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 30,
-  },
-  testButton: {
-    width: 200,
-    height: 100,
-  },
-});
