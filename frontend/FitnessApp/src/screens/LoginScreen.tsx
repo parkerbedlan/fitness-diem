228558be6706a591e7efa33d8b828e63a45db24c
Parker Bedlan <parkerbedlan@gmail.com>
2021-09-20 14:19:34 -0500

    added basic validation to login screen

--- frontend/FitnessApp/src/screens/LoginScreen.tsx
+++ frontend/FitnessApp/src/screens/LoginScreen.tsx
@@ -5,6 +5,7 @@ import { Input, Icon, Text, Button } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
 import CenteredContainer from "../components/CenteredContainer";
 import LineBreak from "../components/LineBreak";
+import * as yup from "yup";
 
 type LoginScreenProps = {
   navigation: any;
@@ -29,8 +30,22 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
             setSubmitting(false);
           }, 1000);
         }}
+        validationSchema={yup.object({
+          email: yup
+            .string()
+            .required("Please enter your email.")
+            .email("Invalid email"),
+          password: yup.string().required("Please enter your password."),
+        })}
       >
-        {({ values, handleChange, handleSubmit, isSubmitting }) => (
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          errors,
+          touched,
+        }) => (
           <>
             <Input
               placeholder="john@example.com"
@@ -38,6 +53,7 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
               onChangeText={handleChange("email")}
               label="Email"
               leftIcon={<Icon name="email" />}
+              errorMessage={touched.email ? errors.email : ""}
             />
             <Input
               placeholder="password123"
@@ -46,6 +62,7 @@ export default function LoginScreen({ navigation }: LoginScreenProps) {
               label="Password"
               secureTextEntry={true}
               leftIcon={<Icon name="lock" />}
+              errorMessage={touched.password ? errors.password : ""}
             />
             <LoginSignupButtonGroup
               onSubmit={handleSubmit}
@@ -71,49 +88,26 @@ const LoginSignupButtonGroup = ({
   version: 1 | 2;
   isLoading: boolean;
 }) => {
-  const versions = {
-    1: (
-      <View
-        style={tw`flex flex-row w-full justify-center items-center justify-around mt-2`}
-      >
+  return (
+    <View style={tw`flex justify-center items-center`}>
+      <Button
+        raised
+        title="Log in"
+        buttonStyle={tw`bg-purple-500 w-44 h-20`}
+        titleStyle={tw`text-3xl p-3`}
+        onPress={(e) => onSubmit()}
+        loading={isLoading}
+        loadingProps={{ size: "large" }}
+      />
+      <View style={tw`mt-1`}>
         <Button
-          raised
+          type="clear"
           title="Don't have an account? Sign Up!"
-          buttonStyle={tw`bg-purple-500 w-44 h-20`}
-          titleStyle={tw`text-lg`}
-          onPress={(e) => console.log("signing up")}
-        />
-        <Button
-          raised
-          title="Log in"
-          buttonStyle={tw`bg-purple-500 w-44 h-20`}
-          titleStyle={tw`text-3xl p-3`}
-          onPress={(e) => onSubmit()}
+          buttonStyle={tw`h-20`}
+          titleStyle={tw`text-lg text-purple-800`}
+          onPress={(e) => onSignup()}
         />
       </View>
-    ),
-    2: (
-      <View style={tw`flex justify-center items-center`}>
-        <Button
-          raised
-          title="Log in"
-          buttonStyle={tw`bg-purple-500 w-44 h-20`}
-          titleStyle={tw`text-3xl p-3`}
-          onPress={(e) => onSubmit()}
-          loading={isLoading}
-          loadingProps={{ size: "large" }}
-        />
-        <View style={tw`mt-1`}>
-          <Button
-            type="clear"
-            title="Don't have an account? Sign Up!"
-            buttonStyle={tw`h-20`}
-            titleStyle={tw`text-lg text-purple-800`}
-            onPress={(e) => onSignup()}
-          />
-        </View>
-      </View>
-    ),
-  };
-  return versions[version];
+    </View>
+  );
 };
