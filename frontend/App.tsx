56e10bcd7e293c7ac654cbc7b9710d91ee3fef0f
Parker Bedlan <parkerbedlan@gmail.com>
2021-11-03 00:00:03 -0500

    purged app and fixed peer dependencies with npm update

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,47 +1,52 @@
 import React from "react";
+import { Text } from "react-native-elements";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ThemeProvider } from "react-native-elements";
-import LoginScreen, { LoginScreenName } from "./src/screens/LoginScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen, { HomeScreenName } from "./src/screens/HomeScreen";
-import SignupScreen, { SignupScreenName } from "./src/screens/SignupScreen";
-import { RootScreenList } from "./src/screens/RootScreensManager";
+import CenteredContainer from "./src/components/CenteredContainer";
+// import { ThemeProvider } from "react-native-elements";
+// import LoginScreen, { LoginScreenName } from "./src/screens/LoginScreen";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import HomeScreen, { HomeScreenName } from "./src/screens/HomeScreen";
+// import SignupScreen, { SignupScreenName } from "./src/screens/SignupScreen";
+// import { RootScreenList } from "./src/screens/RootScreensManager";
 
-const Stack = createNativeStackNavigator<RootScreenList>();
+// const Stack = createNativeStackNavigator<RootScreenList>();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ThemeProvider>
-        <Navigator />
-      </ThemeProvider>
+      {/* <ThemeProvider> */}
+      {/* <Navigator /> */}
+      <CenteredContainer>
+        <Text h1>hi</Text>
+      </CenteredContainer>
+      {/* </ThemeProvider> */}
     </SafeAreaProvider>
   );
 }
 
-const Navigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name={LoginScreenName} component={LoginScreen} />
-        <Stack.Screen
-          name={HomeScreenName}
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen
-          name={SignupScreenName}
-          component={SignupScreen}
-          options={{
-            headerShown: true,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+// const Navigator = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         screenOptions={{
+//           headerShown: false,
+//         }}
+//       >
+//         <Stack.Screen name={LoginScreenName} component={LoginScreen} />
+//         <Stack.Screen
+//           name={HomeScreenName}
+//           component={HomeScreen}
+//           options={{ title: "Welcome" }}
+//         />
+//         <Stack.Screen
+//           name={SignupScreenName}
+//           component={SignupScreen}
+//           options={{
+//             headerShown: true,
+//           }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
