c22b7bc0575fc3e460adc7577ccec7c8780580c9
Parker Bedlan <parkerbedlan@gmail.com>
2021-11-03 17:31:28 -0500

    moved upload test from top of nav stack, gitignored .idea/

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -40,7 +40,6 @@ const Navigator = () => {
           headerShown: false,
         }}
       >
-        <Stack.Screen name={UploadTestName} component={UploadTestScreen} />
         <Stack.Screen name={LoginScreenName} component={LoginScreen} />
         <Stack.Screen
           name={HomeScreenName}
@@ -54,6 +53,7 @@ const Navigator = () => {
             headerShown: true,
           }}
         />
+        <Stack.Screen name={UploadTestName} component={UploadTestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
