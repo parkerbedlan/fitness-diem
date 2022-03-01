349795ad04a02c30ead14e80bda08276653d84ae
Parker Bedlan <parkerbedlan@gmail.com>
2022-03-01 00:03:52 -0600

    added keys to rendered workouts and exercises

--- frontend/src/screens/Workouts/MainWorkoutsScreen.tsx
+++ frontend/src/screens/Workouts/MainWorkoutsScreen.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { Button, Icon, Text } from "react-native-elements";
-import { FlatList, ScrollView, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
-import {List} from 'react-native-paper';
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { List } from "react-native-paper";
 
 //import NewWorkoutOverlay from "../components/NewWorkoutOverlay";
 import tw from "tailwind-react-native-classnames";
@@ -14,96 +21,114 @@ export type MainWorkoutsScreenParams = undefined;
 export const MainWorkoutsScreen = () => {
   //const { navigation, route } = useRootScreen(WorkoutsScreenName);
 
-
-    const [workoutList, setWorkoutList] = useState([    
+  const [workoutList, setWorkoutList] = useState([
+    {
+      id: 1,
+      name: "Chest and Triceps",
+      exercises: [
         {
-            name: "Chest and Triceps",
-            exercises : [
-                {
-                    name: "Bench Press",
-                    sets: 3,
-                    repsPerSet: 10
-                },
-                {
-                    name: "Tricep Pulldowns",
-                    sets: 3,
-                    repsPerSet: 10
-                }
-            ]
+          id: 1,
+          name: "Bench Press",
+          sets: 3,
+          repsPerSet: 10,
         },
         {
-            name: "Back and Biceps",
-            exercises : [
-                {
-                    name: "Lat Pulldowns",
-                    sets: 3,
-                    repsPerSet: 10
-                },
-                {
-                    name: "Bicep Curls",
-                    sets: 3,
-                    repsPerSet: 10
-                }
-            ]
+          id: 2,
+          name: "Tricep Pulldowns",
+          sets: 3,
+          repsPerSet: 10,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Back and Biceps",
+      exercises: [
+        {
+          id: 1,
+          name: "Lat Pulldowns",
+          sets: 3,
+          repsPerSet: 10,
         },
         {
-            name: "Leg Day",
-            exercises : [
-                {
-                    name: "Squat",
-                    sets: 3,
-                    repsPerSet: 10
-                },
-                {
-                    name: "Leg Press",
-                    sets: 3,
-                    repsPerSet: 10
-                }
-            ]
-        }]);
-       
-    function addToWorkoutList(newWorkout:any){
-        var newWorkoutList = workoutList;
-        newWorkoutList.push(newWorkout);
-        setWorkoutList(newWorkoutList);
-        console.log(workoutList);
-    }
+          id: 2,
+          name: "Bicep Curls",
+          sets: 3,
+          repsPerSet: 10,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Leg Day",
+      exercises: [
+        {
+          id: 1,
+          name: "Squat",
+          sets: 3,
+          repsPerSet: 10,
+        },
+        {
+          id: 2,
+          name: "Leg Press",
+          sets: 3,
+          repsPerSet: 10,
+        },
+      ],
+    },
+  ]);
 
-    function renderItem(item: { name: any; exercises: any; }){
-        return(
-            <View style={tw``}>
-                <List.Accordion title={item.name} style={{backgroundColor : "#f0defc"}} titleStyle={{fontSize : 25}}>
+  function addToWorkoutList(newWorkout: any) {
+    var newWorkoutList = workoutList;
+    newWorkoutList.push(newWorkout);
+    setWorkoutList(newWorkoutList);
+    console.log(workoutList);
+  }
 
-                        {[...Array(item.exercises.length)].map(
-                            (value: undefined, index: number) => (
-                                <View>
-                                    <View >
-                                        <List.Item titleStyle={tw``} title={item.exercises[index].name + ": " + item.exercises[index].sets + " sets of " + item.exercises[index].repsPerSet + " reps"}>
-                                        </List.Item>
-                                    </View>
-                                    <View
-                                    style={{
-                                        borderBottomColor: '#cb81fc',
-                                        borderBottomWidth: 1,
-                                    }}
-                                    />
-                                </View>
-                            )
-                        )}
-                </List.Accordion>
-                <View
-                    style={{
-                        borderBottomColor: '#cb81fc',
-                        borderBottomWidth: 3
-                    }}
-                />
-            </View>
-        )
-    }
-
+  function renderItem(item: { name: any; exercises: any; id: any }) {
     return (
-        <View style={tw`h-full`}>
-                {/* <ScrollView>
+      <View key={item.id}>
+        <List.Accordion
+          title={item.name}
+          style={{ backgroundColor: "#f0defc" }}
+          titleStyle={{ fontSize: 25 }}
+        >
+          {item.exercises.map((exercise: any) => (
+            <View key={exercise.id}>
+              <View>
+                <List.Item
+                  title={
+                    exercise.name +
+                    ": " +
+                    exercise.sets +
+                    " sets of " +
+                    exercise.repsPerSet +
+                    " reps"
+                  }
+                ></List.Item>
+              </View>
+              <View
+                style={{
+                  borderBottomColor: "#cb81fc",
+                  borderBottomWidth: 1,
+                }}
+              />
+            </View>
+          ))}
+        </List.Accordion>
+        <View
+          style={{
+            borderBottomColor: "#cb81fc",
+            borderBottomWidth: 3,
+          }}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View style={tw`h-full`}>
+      {/* <ScrollView>
                 
                 <FlatList
                     extraData={visible}
@@ -112,30 +137,23 @@ export const MainWorkoutsScreen = () => {
                 />
                 </ScrollView> */}
 
-                    {[...Array(workoutList.length)].map(
-                        (value: undefined, index: number) => (
-                            renderItem(workoutList[index])
-                        )
-                    )}
+      {workoutList.map((workout) => renderItem(workout))}
 
-
-                <NewPostButton/>
-
-        </View>
+      <NewPostButton />
+    </View>
   );
-}
-
+};
 
 const NewPostButton = () => {
-    const {
-      navigation: { navigate },
-    } = useWorkoutsStackScreen();
-    return (
-      <Button
-        icon={<Icon name="add-comment" color="white" size={40} />}
-        buttonStyle={tw`rounded-full w-20 h-20 bg-purple-600`}
-        containerStyle={tw`absolute bottom-4 right-4`}
-        onPress={() => navigate("NewWorkout")}
-      />
-    );
-  };
+  const {
+    navigation: { navigate },
+  } = useWorkoutsStackScreen();
+  return (
+    <Button
+      icon={<Icon name="add-comment" color="white" size={40} />}
+      buttonStyle={tw`rounded-full w-20 h-20 bg-purple-600`}
+      containerStyle={tw`absolute bottom-4 right-4`}
+      onPress={() => navigate("NewWorkout")}
+    />
+  );
+};
